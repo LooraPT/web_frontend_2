3290e8e314287508d2eb31058b19373578f369eb
LooraPT <102517968+LooraPT@users.noreply.github.com>
2022-10-21 14:22:30 +0300

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,6 +3,8 @@ const basketItems = basket.querySelector('.dropdown__list-basket');
 const shopIcon = document.querySelectorAll('[data-item]');
 data = '';
 
+
+
 if(basket) {
     basket.addEventListener("click", function () {
         basket.classList.toggle('_active')
@@ -16,8 +18,20 @@ shopIcon.forEach((elem) => {
         elem.style.backgroundColor = 'red'
         const a = elem.closest('.shop__hover').querySelector('.shop__text').textContent
         data += `<li class="dropdown__link-basket"><span class="minus__item"></span>${a}</li>`
-        console.log(a)
+        
         basketItems.innerHTML = data;
+
+        sumBasket()
     });
 })
 
+function sumBasket() {
+    const moneyItem = document.querySelectorAll('.col__rano')
+    //data += `<li class="dropdown__link-basket"><span class="minus__item"></span>Sum: </li>`
+    basketItems.insertAdjacentHTML("beforeend", '<li class="dropdown__link-basket"><span class="minus__item"></span>Sum: </li>')
+    
+    moneyItem.forEach((elem) => {
+        a = elem.querySelector('.cena__posiomn').textContent
+        
+    })
+}
