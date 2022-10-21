72d26a1fcf221d7f3a8d20da14dfb64eb40e595b
LooraPT <102517968+LooraPT@users.noreply.github.com>
2022-10-21 11:25:55 +0300

    update

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,6 +1,7 @@
 const basket = document.querySelector('.basket__header--title');
+const basketItems = basket.querySelector('.dropdown__list-basket');
 const shopIcon = document.querySelectorAll('[data-item]');
-
+data = '';
 
 if(basket) {
     basket.addEventListener("click", function () {
@@ -11,6 +12,12 @@ if(basket) {
 
 shopIcon.forEach((elem) => {
     elem.addEventListener("click", function () {
+        
         elem.style.backgroundColor = 'red'
+        const a = elem.closest('.shop__hover').querySelector('.shop__text').textContent
+        data += `<li class="dropdown__link-basket"><span class="minus__item"></span>${a}</li>`
+        console.log(a)
+        basketItems.innerHTML = data;
     });
 })
+
